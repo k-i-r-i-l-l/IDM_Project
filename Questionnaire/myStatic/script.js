c949cef5e59df4307fc58c2b9337d9cf4a7a3b5e
Kirill <chernysk@tcd.ie>
2018-07-06 16:20:10 +0100

    test01 + script.js

--- Questionnaire/myStatic/script.js
+++ Questionnaire/myStatic/script.js
@@ -3,3 +3,18 @@
 function runAds() {
     alert("ad is running");
 }
+
+var answer = ["A"];
+
+function checkAnswers () {
+    var radios = document.getElementsByName("option");
+    console.log(radios);
+    for(var i=0; i<radios.length; i++)
+        if(radios[i].checked) {
+            if (radios[i].value==answer) {
+                alert("you are correct");
+            } else {
+                alert("you are wrong");
+            }
+        }
+}
