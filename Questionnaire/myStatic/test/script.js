e587156b3ccec88ecbce241b176023241e3e63b1
Kirill <chernysk@tcd.ie>
2018-07-19 14:02:43 +0100

    master

---
+++ Questionnaire/myStatic/test/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var answer = ["A"];
+
+function checkAnswers () {
+    var radios = document.getElementsByName("option");
+    console.log(radios);
+    for(var i=0; i<radios.length; i++)
+        if(radios[i].checked) {
+            if (radios[i].value==answer) {
+                alert("you are correct");
+            } else {
+                alert("you are wrong");
+            }
+        }
+}
